063eed47e4ce47a78689446c90d03660989d8d78
RayanneP <rayannepicinidasilva@gmail.com>
2025-04-28 11:26:44 -0300

    .

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//Arquivo de configuração com as constantes do sistema
+
+define('ARQUIVO_JSON' , __DIR__ . '/../data/veiculos.json');
+define('ARQUIVO_USUARIOS' , __DIR__ . '/../data/usuarios.json');
+define('DIARIA_CARRO', 200.00);
+define('DIARIA_MOTO', 80.00);
+?>
